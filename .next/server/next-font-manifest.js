03c29aec8f11e200378c37b610dcc521bab27e19
AlvaroHClaver <alvarohc99@hotmail.com>
2023-12-18 19:21:53 -0300

    rmv .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/mackleaps/Documents/SiteMackleaps/src/app/layout\":[\"static/media/07a54048a9278940-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"x7g+WGwIOcDkXf+3dVltNXaUJsdjx51uSlP9cd+qm1s=\"\n}"
