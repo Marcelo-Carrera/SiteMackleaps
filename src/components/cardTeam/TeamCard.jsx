da65bdb728a401e4d2a82faa35c3019f768a3d5b
ErikaBP <erikaborgessp@gmail.com>
2024-01-16 13:02:51 -0300

    finalização da página de equipe

--- src/components/cardTeam/TeamCard.jsx
+++ src/components/cardTeam/TeamCard.jsx
@@ -12,8 +12,18 @@ export const TeamCard = ({ img, name, role, link, date }) => {
         {role ? <p className={styles.role}>{role}</p> : ""}
         {date ? <p className={styles.date}>{date}</p> : ""}
         <div className={styles.link}>
-          <img src="/link.png" alt="Currículo Lattes" className={styles.icon} />
-          {link ? <p className={styles.role}>{link}</p> : ""}
+          {link ? (
+            <Link href={link} className={styles.lattes} target="_blank">
+              <img
+                src="/link.png"
+                alt="Currículo Lattes"
+                className={styles.icon}
+              />
+              Currículo Lattes
+            </Link>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
