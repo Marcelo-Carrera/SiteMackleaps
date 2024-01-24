0453f546bd38b03342da7c430020a9e29078f23f
AlvaroHClaver <alvarohc99@hotmail.com>
2024-01-24 15:11:47 -0300

    teste2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/mackleaps", // Substitua '/subdominio' pelo seu basePath desejado
+  assetPrefix: "/mackleaps/",
 };
 
 module.exports = nextConfig;
